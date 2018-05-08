b8be4c9b04ac1cac42dd9fb96f669c7ba4df225f
Éléonore <eleonore.mayola@ymail.com>
2018-05-08 12:31:42 +0200

    WIP almost there! Marker + empty map

--- components/MapView.vue
+++ components/MapView.vue
@@ -1,10 +1,19 @@
 <template>
   <div class="map">
-    <img src="~/static/map.png" alt="map of Stockholm"/>
+    <div id="map-wrap" style="height: 100%">
+      <no-ssr>
+	<l-map :zoom=13 :center="[47.413220, -1.219482]">
+	  <l-tilelayer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"></l-tilelayer>
+	  <l-marker :lat-lng="[47.413220, -1.219482]"></l-marker>
+	</l-map>
+      </no-ssr>
+    </div>
   </div>
 </template>
 
 <script>
+import { LMap, LTileLayer, LMarker } from 'nuxt-leaflet';
+
 export default {
   name: 'MapView'
 }
